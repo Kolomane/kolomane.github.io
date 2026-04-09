f5a02e140fd926a7f5d11d5ccd272cff4bc44e6d
Kolomane <mrnameloc@gmail.com>
2026-04-09 00:09:12 -0500

    Custom Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import rehypeExternalLinks from 'rehype-external-links'; 
 
 export default defineConfig({
-  site: 'https://kolomane.github.io',
+  site: 'https://nameloc.net',
   markdown: {                                                                                                                    
       rehypePlugins: [                                                    
         [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],                                              
